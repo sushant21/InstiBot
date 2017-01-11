43b082d4b9e23d46224795e63687348d7c1b1b80
Shashwat Shivam <Shashwat Shivam@DESKTOP-A06QJKV>
2017-01-11 20:45:49 +0530

    m

---
+++ Bot Application1/Bot Application1/Dialogs/SellDialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application1.Dialogs
+{
+    public class SellDialog
+    {
+
+
+        /*
+                // *************************
+                // Log to Database
+                // *************************
+                // Instantiate the BotData dbContext
+                Models.BotDataEntities DB = new Models.BotDataEntities();
+                // Create a new UserLog object
+                Models.UserLog NewUserLog = new Models.UserLog();
+                // Set the properties on the UserLog object
+                NewUserLog.Channel = activity.ChannelId;
+                 NewUserLog.UserID = activity.From.Id;
+            NewUserLog.UserName = activity.From.Name;
+            NewUserLog.created = DateTime.UtcNow;
+            NewUserLog.Message = activity.Text.Truncate(500);
+            // Add the UserLog object to UserLogs
+            DB.UserLogs.Add(NewUserLog);
+            // Save the changes to the database
+            DB.SaveChanges();
+            // Call NumberGuesserDialog
+            await Conversation.SendAsync(activity, () => new NumberGuesserDialog());*/
+    }
+}
