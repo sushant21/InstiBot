91f86ec81378e12711b123fbf5ad018b06f0bb92
Shashwat Shivam <Shashwat Shivam@DESKTOP-A06QJKV>
2017-01-11 20:33:45 +0530

    buy mein dekh

--- Bot Application1/Bot Application1/Dialogs/BuyDialog.cs
+++ Bot Application1/Bot Application1/Dialogs/BuyDialog.cs
@@ -7,5 +7,30 @@ namespace Bot_Application1
 {
     public class BuyDialog
     {
+
+
+
+
+
+/*
+        // *************************
+        // Log to Database
+        // *************************
+        // Instantiate the BotData dbContext
+        Models.BotDataEntities DB = new Models.BotDataEntities();
+        // Create a new UserLog object
+        Models.UserLog NewUserLog = new Models.UserLog();
+        // Set the properties on the UserLog object
+        NewUserLog.Channel = activity.ChannelId;
+         NewUserLog.UserID = activity.From.Id;
+    NewUserLog.UserName = activity.From.Name;
+    NewUserLog.created = DateTime.UtcNow;
+    NewUserLog.Message = activity.Text.Truncate(500);
+    // Add the UserLog object to UserLogs
+    DB.UserLogs.Add(NewUserLog);
+    // Save the changes to the database
+    DB.SaveChanges();
+    // Call NumberGuesserDialog
+    await Conversation.SendAsync(activity, () => new NumberGuesserDialog());*/
     }
 }
